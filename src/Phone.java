7f1c4d1bc067798aeb7b666d6e164b7395ced208
BrandenSandahl <branden157@gmail.com>
2016-02-01 13:16:36 -0500

    Added address field

---
+++ src/Phone.java
@@ -0,0 +1,16 @@
+/**
+ * Created by branden on 2/1/16.
+ *
+ * Should be instantiated as model number
+ */
+public class Phone {
+
+    public double price;
+    public String carrier;
+    public String brand;
+    public int modelYear;
+    public boolean unlocked; //Bool defaults to false, which is probably the answer to this. Leaving as default.
+
+
+
+}
