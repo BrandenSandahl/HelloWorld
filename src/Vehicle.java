9d668f66460863fc39d6c10deafecd9a4ea2b408
BrandenSandahl <branden157@gmail.com>
2016-02-02 20:21:42 -0500

    Made two methods to check for negative or 0 (overloaded methods) added some validation using methods, validation outputs a message.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -33,31 +33,53 @@ public class Vehicle {
         if (isPositive(year)) {
             this.year = year;
         } else {
-            this.year = 1; //this is for testing. This else statement should throw an exception.
+            this.year = -1;
+            System.out.println("There was an error with the year you entered. Please try again");  //this is not a good way to handle this, but at least it gives some feedback
         }
     }
+
     //doors
     public int getDoors() { return doors;  }
-    public void setDoors(int doors) { this.doors = doors; }
+    public void setDoors(int doors) {
+        if (isPositive(doors)) {
+            this.doors = doors;
+        } else {
+            this.doors = -1;
+            System.out.println("There was an error with the doors you entered. Please try again"); //again, bad way to handle this
+        }
+    }
 
     //gaslevel
     public double getGasLevel() { return gasLevel; }
-    public void setGasLevel(double gasLevel) { this.gasLevel = gasLevel;  }
+    public void setGasLevel(double gasLevel) {
+        if (isPositive(gasLevel)) {
+            this.gasLevel = gasLevel;
+        } else {
+            this.gasLevel = -1;
+            System.out.println("There was an error with the gas level. Please try again"); //bad
+        }
+    }
 
     //isAutomatic
     public boolean getAutomatic() { return isAutomatic;  }
-    public void setAutomatic(boolean automatic) { this.isAutomatic = automatic;  }
+    public void setAutomatic(boolean automatic) {
+        if (isAutomatic != true && isAutomatic != false) {
+            System.out.println("You entered an invalid value. Valid values are true or false. Thanks");   //not really possible to trigger this because java would throw an error anyway
+        } else {
+            this.isAutomatic = automatic;
+        }
+    }
 
     /** end of getters and setters **/
 
-    //Method to check for positive numbers
+    //Method to check for positive numbers, including 0
     public boolean isPositive(int numberToCheck) {
         if (numberToCheck >= 0) {
             return true;
         } else {
             return false; }
     }
-
+    //overloading on double
     public boolean isPositive(double numberToCheck) {
         if (numberToCheck >= 0) {
             return true;
