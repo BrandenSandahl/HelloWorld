b1ea72494db2bfda56a631a3ca2b5c5621e382e2
BrandenSandahl <branden157@gmail.com>
2016-02-25 10:36:40 -0500

    added Exercise 06

---
+++ src/exercise06.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ * Created by branden on 2/25/16 at 09:58.
+ */
+public class exercise06 {
+
+
+    public static void main(String[] args) {
+
+        String[] names = {"Alice", "Smith", "Bob", "Hope", "Charlie", "Brown"};
+
+        //reverse the list
+        //turn list into hashmap
+        // {"smith": "alice", "hope": "bob", "brown":"charlie"}
+
+
+
+
+
+        ArrayList<String> reversedNames = new ArrayList<>(Arrays.asList(names));
+        Collections.reverse(reversedNames);
+
+        HashMap<String, String> nameMap = new HashMap<>();
+
+        for (int i = 0; i < reversedNames.size(); i+=2) {
+            nameMap.put(reversedNames.get(i), reversedNames.get(i + 1));
+        }
+
+        /**  MY WAY
+
+        HashMap<String, String> nameMap = new HashMap<>();
+
+        for (int i = names.length - 1  ; i > 0; i -= 2  ) {
+            nameMap.put(names[i], names[i-1]);
+
+        }
+
+         */
+
+
+
+
+
+
+        System.out.println("Stop me");
+
+    }
+
+
+}
